dcfcfa0545d4b86b5953044ae732bff165dbc681
IyadA <iyada@radware.com>
2023-05-24 09:25:10 +0300

    init the BE project

---
+++ src/test/java/com/img/authapp/AuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.img.authapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
